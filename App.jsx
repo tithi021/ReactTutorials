395e4d2e37f1e209f8c18f51373514675e747f31
tithi021 <afroza021@gmail.com>
2016-11-15 15:59:50 +0600

    first-commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Hello React Developers!!!</h1><br />
+            <img src="image/react.png" />
+         </div>
+      );
+   }
+}
+
+export default App;
